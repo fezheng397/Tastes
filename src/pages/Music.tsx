080547bde94f8f78118b79222088109a56d3d3a0
fezheng397 <fezheng397@gmail.com>
2020-08-30 18:41:52 -0400

    added desc

--- src/pages/Music.tsx
+++ src/pages/Music.tsx
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 import { ContentWrapper } from './presenters';
 import { useSpotifyApi } from '../hooks/spotifyHook';
 import { Playlist } from 'components/Spotify';
+import { Header, Text } from 'components/Typography';
+import { theme } from 'constants/theme';
 
 interface MusicProps {}
 
@@ -15,6 +17,14 @@ const Music: React.FC<MusicProps> = () => {
   const playlists = useSpotifyApi(playlistIds);
   return (
     <ContentWrapper>
+      <Description>
+        <PageHeader>
+          <Header as='h1'>Our Favorite Playlists</Header>
+        </PageHeader>
+        <Text size='lg' color={theme.text.light56} weight={500}>
+          A compilation of music Aditya and I have discovered over the years.
+        </Text>
+      </Description>
       {playlists.length > 0 &&
         playlists.map((playlist) => (
           <PlaylistWrapper>
@@ -27,6 +37,15 @@ const Music: React.FC<MusicProps> = () => {
 
 const PlaylistWrapper = styled.div`
   padding: 20px;
+`;
+
+const Description = styled.div`
+  text-align: center;
+  padding: 32px;
+`;
+
+const PageHeader = styled.div`
+  margin-bottom: 20px;
 `;
 
 export default Music;
